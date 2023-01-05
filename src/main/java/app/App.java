6d8d16ae70b2e7fe86776120a183e8a8726b5f6c
Matthias Heimberg <matthias.heimberg@students.ffhs.ch>
2023-01-05 15:17:34 +0100

    add counters for prometheus

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -10,7 +10,8 @@ public class App {
         PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
         io.micrometer.core.instrument.Counter userApiCounter = prometheusRegistry.counter("user_api_counter");
         io.micrometer.core.instrument.Counter getUserByIDCounter = prometheusRegistry.counter("get_user_by_id_counter");
-       
+        io.micrometer.core.instrument.Counter postNewUserCounter = prometheusRegistry.counter("post_new_user_counter");
+        io.micrometer.core.instrument.Counter deleteUserCounter = prometheusRegistry.counter("delete_user_counter");
 
         var users = new UserRepository();
 
@@ -37,12 +38,14 @@ public class App {
         });
 
         app.delete("api/users/{id}", ctx -> {
+            deleteUserCounter.increment();
             var id = Integer.valueOf(ctx.pathParam("id"));
             users.delete(id);
             ctx.status(204);
         });
 
         app.post("api/users", ctx -> {
+            postNewUserCounter.increment();
             var user = ctx.bodyAsClass(User.class);
             var newUser = users.save(user);
             ctx.json(newUser);
